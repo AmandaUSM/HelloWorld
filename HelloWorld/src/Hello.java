ef57eda4e0b28f7c13e380edaefe23b341bc7192
AmandaUSM <amanda.l.castonguay@maine.edu>
2017-01-30 16:36:07 -0500

    Added announcement message
    Signed-off-by: AmandaUSM <amanda.l.castonguay@maine.edu>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,7 @@
 
 public class Hello {
    public static void main(String[] args){
-      System.out.println("Hello World!");
+      System.out.println("Here ye Here ye : important announcement");
+      System.out.println("Hello World!");  
    }
 }

19df99c67b205748f2936d7664b8b37cb6edc13c
AmandaUSM <amanda.l.castonguay@maine.edu>
2017-01-30 16:36:59 -0500

    Added exit message

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
    public static void main(String[] args){
       System.out.println("Here ye Here ye : important announcement");
       System.out.println("Hello World!");  
+      System.out.println("That is all for now.");
    }
 }

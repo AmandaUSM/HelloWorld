3197a25949b2263996926ffefe38542b62d57624
AmandaUSM <amanda.l.castonguay@maine.edu>
2017-02-06 16:37:03 -0500

    Added date message

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,8 +1,15 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Hello {
    public static void main(String[] args){
       System.out.println("Here ye Here ye : important announcement");
 
+      DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+      Date date = new Date();
+      System.out.println(dateFormat.format(date));
+      
       System.out.println("Hello World!");
       System.out.println("Listen.");
       System.out.println("Look around.");
